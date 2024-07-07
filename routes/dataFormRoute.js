be0b544b6101ff2efa1e2e3ca94538e4ff825c6f
usuarioo1 <nicolasgonzalezmoller@gmail.com>
2024-07-07 18:56:33 -0400

    data

--- routes/dataFormRoute.js
+++ routes/dataFormRoute.js
@@ -1,10 +1,13 @@
-const express = require('express')
+const express = require('express');
 
-const {getDataForm, getDataFormById, createDataForm} = require('../controllers/dataformController')
+const { getDataForm, getDataFormById, createDataForm } = require('../controllers/dataformController');
 const dataFormRoute = express.Router();
 
-dataFormRoute.route('/dataform').get(getDataForm);
-dataFormRoute.route('/dataform/:id').get(getDataFormById)
-dataFormRoute.route('/createdataform').post(createDataForm)
+dataFormRoute.route('/dataform')
+    .get(getDataForm)
+    .post(createDataForm);
+
+dataFormRoute.route('/dataform/:id')
+    .get(getDataFormById);
 
 module.exports = dataFormRoute;
